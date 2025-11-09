39119add5b51874812bfe51eba3e75d142f53788
Hheojiwon <0618amy@naver.com>
2025-11-09 22:03:31 +0900

    Initial commit

---
+++ src/test/java/com/example/mudy/MudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
