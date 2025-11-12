a1e22a8af5db94051db22db42332755fe524dcb2
Hheojiwon <0618amy@naver.com>
2025-11-13 02:33:08 +0900

    feat: 곡 재생 및 큐 로직 처리 클래스

---
+++ src/main/java/com/example/mudy/music/handler/TrackScheduler.java
@@ -0,0 +1,47 @@
+package com.example.mudy.music.handler;
+
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+import com.sedmelluq.discord.lavaplayer.player.event.AudioEventAdapter;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrackEndReason;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+
+//  트랙 재생 순서를 관리하는 스케줄러, 큐 관리자
+public class TrackScheduler extends AudioEventAdapter {
+    private final AudioPlayer player;
+    private final BlockingQueue<AudioTrack> queue;
+
+    public TrackScheduler(AudioPlayer player) {
+        this.player = player;
+        this.queue = new LinkedBlockingQueue<>();
+    }
+
+    public void queue(AudioTrack track) {
+        // startTrack의 noInterrupt가 true이면, 현재 재생 중이 아닐 때만 시작
+        if (!player.startTrack(track, true)) {
+            queue.offer(track);
+        }
+    }
+
+    public void nextTrack() {
+        player.startTrack(queue.poll(), false);
+    }
+
+    @Override
+    public void onTrackEnd(AudioPlayer player, AudioTrack track, AudioTrackEndReason endReason) {
+        if (endReason.mayStartNext) {
+            nextTrack();
+        }
+    }
+
+    public int getQueueSize() {
+        return queue.size();
+    }
+
+    public void clearQueue() {
+        queue.clear();
+    }
+}
