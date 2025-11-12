bd488a5fd2a6777c6e069d313b434cd31dddc927
Hheojiwon <0618amy@naver.com>
2025-11-13 02:32:31 +0900

    feat: lavaplayer 오디오 전송을 위한 필수 핸들러 작성

---
+++ src/main/java/com/example/mudy/music/handler/AudioPlayerSendHandler.java
@@ -0,0 +1,38 @@
+package com.example.mudy.music.handler;
+
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+import com.sedmelluq.discord.lavaplayer.track.playback.MutableAudioFrame;
+import net.dv8tion.jda.api.audio.AudioSendHandler;
+
+import java.nio.ByteBuffer;
+
+ // Discord에 오디오 데이터를 전송하는 핸들러
+public class AudioPlayerSendHandler implements AudioSendHandler {
+
+    private final AudioPlayer audioPlayer;
+    private final ByteBuffer buffer;
+    private final MutableAudioFrame frame;
+
+    public AudioPlayerSendHandler(AudioPlayer audioPlayer) {
+        this.audioPlayer = audioPlayer;
+        this.buffer = ByteBuffer.allocate(1024);
+        this.frame = new MutableAudioFrame();
+        this.frame.setBuffer(buffer);
+    }
+
+    @Override
+    public boolean canProvide() {
+        buffer.clear();
+        return audioPlayer.provide(frame);
+    }
+
+    @Override
+    public ByteBuffer provide20MsAudio() {
+        return buffer.flip();
+    }
+
+    @Override
+    public boolean isOpus() {
+        return true;
+    }
+}
