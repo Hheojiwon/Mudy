96328ce954d046f57095829a3a3207e5ad910e7f
Hheojiwon <0618amy@naver.com>
2025-11-14 18:09:30 +0900

    feat: 일시정지, 다음곡 command enum 추가

--- src/main/java/com/example/mudy/music/command/MusicCommand.java
+++ src/main/java/com/example/mudy/music/command/MusicCommand.java
@@ -10,7 +10,12 @@ public enum MusicCommand {
 
     PLAY("재생", "공부하기 좋은 조용한 음악을 재생합니다."),
 
-    STOP("정지", "음악 재생을 멈추고 큐를 비웁니다.");
+    STOP("정지", "음악 재생을 멈추고 큐를 비웁니다."),
+
+    PAUSE("일시정지", "음악을 일시 정지 합니다."),
+
+    SKIP("다음곡", "현재 곡을 건너뛰고 다음 곡을 재생합니다.");
+
 
     private final CommandData commandData;
 
